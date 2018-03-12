7c22d37a7278413e99bd5bd280a19c2846cdd41a
Blessing Orazulume <blessing.orazulume@andela.com>
2018-03-12 23:08:24 +0100

    rube game setup

---
+++ Assets/ControllerInputManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerInputManager : MonoBehaviour {
+	public SteamVR_TrackedObject trackedObj;
+	public SteamVR_Controller.Device device;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		device = SteamVR_Controller.Input ((int)trackedObj.index);
+	}
+}
